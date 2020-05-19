80d0b316a28b522b0106f1cdba12d9df20065148
Bryan Vallejo <bryan0289@hotmail.com>
2020-05-19 09:14:47 -0500

    Deber

--- proyecto-angular/src/app/app.module.ts
+++ proyecto-angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DeberComponent } from './deber/deber.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeberComponent
   ],
   imports: [
     BrowserModule
---
+++ proyecto-angular/src/app/deber/deber.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deber',
+  templateUrl: './deber.component.html',
+  styleUrls: ['./deber.component.css']
+})
+export class DeberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
